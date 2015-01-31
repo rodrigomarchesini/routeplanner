222daf721b49dfb6439547431b2f3d0085c64c92
Rodrigo Marchesini <marchesini@gmail.com>
2015-01-31 14:45:21 -0200

    Base project

---
+++ routeplanner-model/src/test/java/com/walmart/routeplanner/model/AppTest.java
@@ -0,0 +1,11 @@
+package com.walmart.routeplanner.model;
+
+import org.junit.Test;
+
+public class AppTest {
+    
+    @Test
+    public void test() {
+        System.out.println("model test");
+    }
+}
---
+++ routeplanner-services/src/test/java/com/walmart/routeplanner/services/AppTest.java
@@ -0,0 +1,11 @@
+package com.walmart.routeplanner.services;
+
+import org.junit.Test;
+
+public class AppTest {
+    
+    @Test
+    public void test() {
+        System.out.println("services test");
+    }
+}
